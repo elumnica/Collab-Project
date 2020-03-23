eaf6440f3457d6175f43c3cd09074607fcf27f19
Lukewarm <j.addamo@columbia.edu>
2020-03-22 23:40:03 -0400

    retreiving data and appending it to bird-dex.. still in progress

--- javascript.js
+++ javascript.js
@@ -8,9 +8,11 @@
 // Variables
 var queryParams = { "api-key": "vtvt2pqn28os" };
 var queryURL = "https://api.ebird.org/v2/data/obs/" + regionCode + "/recent/notable?detail=full";
-var regionCode = "NY";
+var regionCode = "US-NY";
 
-// var carouselElement = $("carousel").attr();
+
+
+
 
 
 // TODO
@@ -33,15 +35,97 @@ var settings2 = {
 
 
 
+// var geoQuery = {
+//   "url": "https://api.ebird.org/v2/ref/hotspot/US-NY/",
+//   "method": "GET",
+//   "timeout": 0,
+// };
+
+// $.ajax(geoQuery).done(function (response) {
+//   console.log(response);
+// });
+
+
 var geoQuery = {
-  "url": "https://api.ebird.org/v2/ref/hotspot/US-NY/",
+  "url": "https://api.ebird.org/v2/data/obs/US-NY/recent",
   "method": "GET",
   "timeout": 0,
+  "headers": {
+    "X-eBirdApiToken": "vtvt2pqn28os"
+  },
 };
+let temp = [];
+$.ajax(geoQuery).done(function(response) {
+  // console.log(response);
+  let nyBirds = {};
+  let locations = {};
+  let bName= [];
+  let region = [];
+  let coord = [];
+  for(let i=0; i< response.length; i++){
+    locations[response[i].locName] = `${response[i].lat} , ${response[i].lng}`
+    nyBirds[response[i].comName] = locations
+    bName.push(response[i].comName)
+    region.push(response[i].locName)
+    coord.push(`${response[i].lat},${response[i].lng}`)
 
-$.ajax(geoQuery).done(function (response) {
-  console.log(response);
-});
+    bName.forEach(birdFunc);
+
+  }
+
+  // region.forEach(birdFunc);
+  // coord.forEach(birdFunc);
+  
+  // console.log(bName)
+  // console.log(region)
+  // console.log(coord)
+  // console.log(nyBirds)
+
+  
+
+  // function birdFunc(item, index) {
+  //   // console.log(item,index);
+  //   var table = $(".bird-table");
+  //   var tableRowElement = $("<tr class='hoverable'>");
+  //   var tableDataElement = $("<td>").attr(id=`${index}`);
+  //   tableRowElement.append(tableDataElement);
+  //   table.append(tableRowElement);
+  // }
+ 
+
+
+    // function birdFunc(item, index){
+
+    //   var $table = $("#bird-table");
+
+    //   var $tableRowElement = $("<tr class='hoverable'>");
+    //   var $tableDataElement = $("<td>").text(item);
+
+    //   var createTableDataElement = $tableRowElement.append($tableDataElement);
+ 
+
+    //   $tableDataElement.attr(id=index);
+      
+    //   $table.append(createTableDataElement);
+
+    // }
+
+
+  
+
+
+
+
+
+
+      
+    });
+
+
+
+
+
+
 
 
 
